f886a5481d2e4b14600d6e712a8373cabf2c782a
louis-bourriague <louis.bourriague@gmail.com>
2015-01-30 17:04:44 +0100

    commande créer client

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -2,23 +2,23 @@ package controller;
 
 import java.util.ArrayList;
 
+import model.Client;
 import model.Consultant;
-
 import service.ConsultantService;
 import view.MainView;
 
 public class MainController {
-
     private MainView mainView;
-
+    ArrayList<Client> listeClients = new ArrayList<Client>();
     public MainController(MainView mainView) {
         super();
         this.mainView = mainView;
+
     }
 
     public void commande(String commande) {
 
-		String[] splitCommande = commande.split(" ");
+		String[] splitCommande = commande.split(";");
 
         if (splitCommande[0].equals("listeconsultant")) {
             ArrayList<Consultant> listeConsultants = ConsultantService.listeConsultants();
@@ -26,6 +26,19 @@ public class MainController {
             for (Consultant consultant : listeConsultants) {
                 mainView.afficher(consultant.toString());
             }
+
+        } else if (splitCommande[0].equals("creerclient")) {
+            if(splitCommande.length == 5) {
+                Client nouveauClient = new Client(splitCommande[1], splitCommande[2], splitCommande[3], splitCommande[4]);
+
+                listeClients.add(nouveauClient);
+
+                for (Client client : listeClients) {
+                    mainView.afficher("Liste des clients actuels : ");
+                    mainView.afficher(client.toString());
+                }
+            }
+            else  mainView.afficher("syntaxe incorrecte : creerclient;nom;prenom;adresse;telephone ");
 
         } else if (splitCommande[0].equals("listeconsultantlibre")) {
             mainView.afficher("Liste des consultants actuellement libre :");
