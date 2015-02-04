d6b253e94d3eaf1adad6cdbabec86faa81eb2750
JeremyM <meignanjeremy@hotmail.fr>
2015-02-04 17:23:11 +0100

    enregistrer les listes dans des fichier et les lire a l'ouverture

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -8,6 +8,7 @@ import org.joda.time.format.DateTimeFormatter;
 import service.ConsultantService;
 import view.MainView;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Optional;
 
@@ -27,10 +28,27 @@ public class MainController {
         this.mainView = mainView;
 
         consultants = ConsultantService.listeConsultants();
+        File f = new File("consultant.itl");
+        if (f.exists()) {
+            this.consultants = lireConsultant();
+        } else {
+            this.consultants = listeConsultants();
+        }
 
-        this.consultants = listeConsultants();
-        this.missions = new ArrayList<Mission>();
-        this.clients = new ArrayList<Client>();
+        f = new File("cission.itl");
+        if (f.exists()) {
+            this.missions = lireMission();
+        } else {
+            this.missions = new ArrayList<Mission>();
+        }
+
+        f = new File("client.itl");
+        if (f.exists()) {
+            this.clients = lireClient();
+        } else {
+            this.clients = new ArrayList<Client>();
+        }
+
     }
 
     public void commande(String commande) {
@@ -47,6 +65,7 @@ public class MainController {
                 Client nouveauClient = new Client(splitCommande[1], splitCommande[2], splitCommande[3], splitCommande[4]);
 
                 clients.add(nouveauClient);
+                enregistrerListeClient();
 
                 for (Client client : clients) {
                     mainView.afficher("Liste des clients actuels : ");
@@ -59,7 +78,11 @@ public class MainController {
         } else if (splitCommande[0].equals("creerconsultant")) {
             if (splitCommande.length == 5) {
                 consultants.add(new Consultant(splitCommande[1], splitCommande[2], splitCommande[3], splitCommande[4]));
-
+                try {
+                    enregistrerListeConsultant();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             } else {
                 afficherSyntaxe(splitCommande[0]);
             }
@@ -95,7 +118,7 @@ public class MainController {
                             clientMission.get());
 
                     missions.add(mission);
-
+                    enregistrerListeMission();
                     mainView.afficher("Mission ajoutée\n" + mission);
 
                     break;
@@ -111,7 +134,7 @@ public class MainController {
                             client.get());
 
                     missions.add(nouvelleMission);
-
+                    enregistrerListeMission();
                     mainView.afficher("Mission ajoutée\n" + nouvelleMission);
 
                     break;
@@ -141,4 +164,111 @@ public class MainController {
             mainView.afficher("Il y a une erreur de syntaxe. Commande : creerconsultant;nom;prenom;adresse;telephone");
         }
     }
+
+    private void enregistrerListeConsultant() throws IOException {
+        try {
+            OutputStream file = new FileOutputStream("consultant.itl");
+            OutputStream buffer = new BufferedOutputStream(file);
+            ObjectOutput output = new ObjectOutputStream(buffer);
+            output.writeObject(consultants);
+            output.close();
+            buffer.close();
+            file.close();
+        } catch (IOException ex) {
+            mainView.afficher("Certaines données n'ont pas pu être enregistrées, il se peut que des données soient perdues.");
+            ex.printStackTrace();
+        }
+
+    }
+
+    private void enregistrerListeClient() {
+
+        try {
+            OutputStream file = new FileOutputStream("Client.itl");
+            OutputStream buffer = new BufferedOutputStream(file);
+            ObjectOutput output = new ObjectOutputStream(buffer);
+            output.writeObject(clients);
+            output.close();
+            buffer.close();
+            file.close();
+        } catch (IOException ex) {
+            mainView.afficher("Certaines données n'ont pas pu être enregistrées, il se peut que des données soient perdues.");
+        }
+
+    }
+
+    private void enregistrerListeMission() {
+        try {
+            OutputStream file = new FileOutputStream("Mission.itl");
+            OutputStream buffer = new BufferedOutputStream(file);
+            ObjectOutput output = new ObjectOutputStream(buffer);
+            output.writeObject(missions);
+            output.close();
+            buffer.close();
+            file.close();
+        } catch (IOException ex) {
+            mainView.afficher("Certaines données n'ont pas pu être enregistrées, il se peut que des données soient perdues.");
+        }
+
+
+    }
+
+    private ArrayList<Consultant> lireConsultant() {
+        ArrayList<Consultant> transfert = new ArrayList<Consultant>();
+        try {
+            InputStream file = new FileInputStream("Consultant.itl");
+            InputStream buffer = new BufferedInputStream(file);
+            ObjectInput input = new ObjectInputStream(buffer);
+            transfert = (ArrayList<Consultant>) input.readObject();
+            input.close();
+            buffer.close();
+            file.close();
+
+        } catch (IOException ex) {
+            mainView.afficher("Certaines données n'on pas pus être lue, il peut manquer certaine information--- IOException.");
+        } catch (ClassNotFoundException ex) {
+            mainView.afficher("Certaines données n'on pas pus être lue, il peut manquer certaine information--- ClassNotFoundException");
+        }
+        return transfert;
+    }
+
+    private ArrayList<Mission> lireMission() {
+        ArrayList<Mission> transfert = new ArrayList<Mission>();
+        try {
+            InputStream file = new FileInputStream("Mission.itl");
+            InputStream buffer = new BufferedInputStream(file);
+            ObjectInput input = new ObjectInputStream(buffer);
+            transfert = (ArrayList<Mission>) input.readObject();
+            input.close();
+            buffer.close();
+            file.close();
+
+        } catch (IOException ex) {
+            mainView.afficher("Certaines données n'on pas pus être lue, il peut manquer certaine information--- IOException.");
+        } catch (ClassNotFoundException ex) {
+            mainView.afficher("Certaines données n'on pas pus être lue, il peut manquer certaine information--- ClassNotFoundException");
+        }
+        return transfert;
+    }
+
+    private ArrayList<Client> lireClient() {
+        ArrayList<Client> transfert = new ArrayList<Client>();
+        try {
+            InputStream file = new FileInputStream("Client.itl");
+            InputStream buffer = new BufferedInputStream(file);
+            ObjectInput input = new ObjectInputStream(buffer);
+            transfert = (ArrayList<Client>) input.readObject();
+            input.close();
+            buffer.close();
+            file.close();
+
+        } catch (IOException ex) {
+            mainView.afficher("Certaines données n'on pas pus être lue, il peut manquer certaine information--- IOException.");
+        } catch (ClassNotFoundException ex) {
+            mainView.afficher("Certaines données n'on pas pus être lue, il peut manquer certaine information--- ClassNotFoundException");
+        }
+        return transfert;
+    }
+
+
 }
